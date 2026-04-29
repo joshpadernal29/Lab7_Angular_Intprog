71c5cbe411188ee6f84f9eadea3953d286a7f331
joshpadernal29 <josh.padernal29@gmail.com>
2026-04-29 23:16:04 +0800

    added app.initializer.ts

---
+++ my-app/src/app/_helpers/app.initializer.ts
@@ -0,0 +1,11 @@
+// _helpers/app.intializer.ts
+import { catchError, of } from "rxjs";
+import { AccountService } from '@app/_services';
+
+export function appInitializer(accountService: AccountService) {
+    return () => accountService.refreshToken()
+        .pipe(
+            // catch error to start app on success or failure
+            catchError(() => of())
+        );
+}
