21f01cc91d81746709d3c0e40a54f636e0af8872
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-01 01:31:24 +0800

    fixed code typos and

--- my-app/src/app/account/account.module.ts
+++ my-app/src/app/account/account.module.ts
@@ -1,9 +1,8 @@
-// account/account.module.ts
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { AccountRoutingModule } from '../admin/accounts/account-routing.module';
+import { AccountRoutingModule } from './account-routing.module';
 import { LayoutComponent } from './layout.component';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
