9c82f6fc449c10eb484c0a833d60d39ba3f3ba6d
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-01 23:12:55 +0800

    added admin/overview.component.html and ts

---
+++ my-app/src/app/admin/overview.component.ts
@@ -0,0 +1,5 @@
+// admin/overview.component.ts
+import { Component } from "@angular/core";
+
+@Component({ templateUrl: 'overview.component.html', standalone: false })
+export class OverviewCompoment { }
