26eb30335e466debbb39dcafdea502ac9262ea65
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-03 22:21:55 +0800

    added src/main.ts

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+// src/main.ts
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+
+import 'zone.js';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule, { ngZone: 'zone.js' })
+  .catch(err => console.error(err));
