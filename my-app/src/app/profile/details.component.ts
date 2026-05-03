592b8abe895a00976fc19bfe9ef5ef220955bc29
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-03 21:23:53 +0800

    added  profile/details.component.html and ts

---
+++ my-app/src/app/profile/details.component.ts
@@ -0,0 +1,12 @@
+// profile/details.component.ts
+import { Component } from "@angular/core";
+import { AccountService } from "@app/_services";
+
+@Component({ templateUrl: 'details.component.html', standalone: false })
+export class DetailsComponent {
+    constructor(private accountService: AccountService) { }
+
+    get account() {
+        return this.accountService.accountValue;
+    }
+}
