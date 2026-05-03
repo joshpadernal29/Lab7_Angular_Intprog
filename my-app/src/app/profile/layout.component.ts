15a2fe7d21511328b6af3d60efeffa13af771b8d
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-03 21:27:52 +0800

    added profile/layout.component.html and ts

---
+++ my-app/src/app/profile/layout.component.ts
@@ -0,0 +1,5 @@
+// profile/layout.component.ts
+import { Component } from "@angular/core";
+
+@Component({ selector: 'app-profile-layout', templateUrl: 'layout.component.html', standalone: false })
+export class LayoutComponent { }
