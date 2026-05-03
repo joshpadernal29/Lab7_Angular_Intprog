d088253ba134ff86a1aaa2d42ecf41ffd759eda3
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-04 02:03:06 +0800

    fixed import path

--- my-app/src/app/account/account.module.ts
+++ my-app/src/app/account/account.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { AccountRoutingModule } from './account-routing.module';
+import { AccountRoutingModule } from './acount-routing.module';
 import { LayoutComponent } from './layout.component';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
