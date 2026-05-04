e3f50bf6ca59bbec4bfe0632a24756c9c127faa1
joshpadernal29 <josh.padernal29@gmail.com>
2026-05-05 01:38:58 +0800

    added src/test.ts

---
+++ my-app/src/test.ts
@@ -0,0 +1,11 @@
+// src/test.ts
+import { getTestBed } from '@angular/core/testing';
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+);
